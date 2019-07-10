ea6284b0733a93d058ce23bf4e8c3b983da5464e
eta <eta@theta.eu.org>
2019-07-10 16:06:42 +0100

    Sorter docs

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,6 +1,20 @@
 //sorter class implements bubble sort for the blocks and returns instructions for the roobot
 
-
-
 public class Sorter {
+    public static BlockSwitcher switcher = new BlockSwitcher();
+
+    /**
+     * Given a list specifying which order the blocks are in,
+     * issue commands to the BlockSwitcher to sort the blocks
+     * into the correct order (i.e. [1, 2, 3, 4]).
+     *
+     * For example, this might be called as sortBlocks([3, 4, 1, 2]),
+     * and it would then run bubblesort and issue commands to the
+     * BlockSwitcher to switch the blocks back into [1, 2, 3, 4] order.
+     *
+     * @param block_order Which order the blocks are in.
+     */
+    public static void sortBlocks(int[] block_order) {
+
+    }
 }
