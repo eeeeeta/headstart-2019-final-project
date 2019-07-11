64fdba0fcdf7e0f4f20ff8b720589c647ccc32f8
eta <eta@theta.eu.org>
2019-07-11 09:36:34 +0100

    add ColourDetector class, and add DISTANCE_BETWEEN_BLOCKS constant

---
+++ src/ColourDetector.java
@@ -0,0 +1,16 @@
+/**
+ * Detects the colours of all the blocks at the very start.
+ */
+public class ColourDetector {
+    public static CoordinateManager coord = new CoordinateManager();
+
+    /**
+     * Detect the colours of the blocks, at the very start of the program,
+     * and return a list of which order the blocks are in.
+     *
+     * @return
+     */
+    public static int[] detectColours() {
+        throw new Error(); // not yet implemented
+    }
+}
